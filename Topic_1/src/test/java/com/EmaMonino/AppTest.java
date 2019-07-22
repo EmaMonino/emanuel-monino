c07cb19bd5a6764eed3bd8427ca62017957c0373
EmaMonino <moninoemanuel@Gmail.com>
2019-07-22 09:48:21 -0300

    topic 1 terminado

---
+++ Topic_1/src/test/java/com/EmaMonino/AppTest.java
@@ -0,0 +1,16 @@
+package com.EmaMonino;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest 
+{
+
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        System.out.println( "Hello World!" );
+        assertTrue( true );
+    }
+}
